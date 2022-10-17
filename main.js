4de09089f82d04067f7bd591aa697147a4de7eb2
nelkonbob2002 <nelkonbob2002@yahoo.com>
2022-10-17 23:57:10 +0200

    first time uploading to git through vscode

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let succeeded = true;
+
+// function sayHi() {
+//     return new Promise((resolve, reject) =>{
+
+//         setTimeout(() => {console.log('Hello my friend!!! How are you?')
+//         if (succeeded){
+//             resolve()   
+//            } else{
+//                reject()
+//            }}, 2000)
+//     })
+// }
+//   function response() {
+//     console.log("I'm good, thank you very much!!!")
+//   };
+  
+//   sayHi().then(() => response()).catch(() => console.log('rejected'))
